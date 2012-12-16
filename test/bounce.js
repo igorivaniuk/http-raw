49eb357c1444bbc4af0c3057697d205a8479c40a
James Halliday <mail@substack.net>
2012-12-15 19:07:21 -0800

    failing bounce test

---
+++ test/bounce.js
@@ -0,0 +1,41 @@
+var createServer = require('../');
+var test = require('tap').test;
+
+var http = require('http');
+var net = require('net');
+
+var server = createServer(function (req, res) {
+    var rs = req.createRawStream();
+    rs.pipe(process.stdout,{end:false});
+    rs.pipe(net.connect(7001)).pipe(rs);
+});
+server.listen(0);
+var port = server.address().port;
+
+var target = http.createServer(function (req, res) {
+    res.end('beep boop\n');
+});
+target.listen(7001);
+
+server.on('listening', function () {
+    test('bounce a request', testBounce);
+    test(function (t) {
+        server.close();
+        target.close();
+        t.end();
+    });
+});
+
+function testBounce (t) {
+    t.plan(2);
+    
+    var c = net.connect(port);
+    var data = '';
+    c.on('data', function (buf) { data += buf });
+    c.on('end', function () {
+        var parts = data.split(/\r\n\r\n|\n\n/);
+        t.ok(/^HTTP\/1.1 200 OK/.test(parts[0]));
+        t.equal(parts[1], 'beep boop\n');
+    });
+    c.end('GET / HTTP/1.1\r\nHost: localhost\r\n\r\n');
+}
