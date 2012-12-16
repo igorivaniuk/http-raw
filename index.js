a1daee679dda2a593c81e295ffac93fc3253d005
James Halliday <mail@substack.net>
2012-12-16 15:52:36 -0800

    split createStream() into its own file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var https = require('https');
-var Stream = require('stream');
+var createStream = require('./lib/create_stream');
 
 exports = module.exports = fromServer(http.createServer);
 exports.http = fromServer(http.createServer);
@@ -102,52 +102,4 @@ function injectRaw (con, incoming) {
         con._rawBuffers = undefined;
     });
     
-}
-
-function createStream (con, incoming) {
-    if (con.parser) {
-        con.parser.onerror = function () {};
-    }
-    con.ondata = function () {};
-    
-    incoming.upgradeOrConnect = true;
-    incoming.upgrade = true;
-    incoming.shouldKeepAlive = true;
-    
-    var s = new Stream;
-    s.writable = true;
-    s.readable = true;
-    
-    var c = incoming.connection;
-    
-    s.write = c.write.bind(c);
-    s.end = c.end.bind(c);
-    s.destroy = c.destroy.bind(c);
-    s.pause = c.pause.bind(c);
-    s.resume = c.resume.bind(c);
-    
-    c.on('drain', function () { s.emit('drain') });
-    
-    c.on('end', function () {
-        setTimeout(function () {
-            s.emit('end');
-            close();
-        }, 100);
-    });
-    c.on('close', close);
-    
-    var closed = false;
-    function close () {
-        if (closed) return;
-        s.emit('close');
-        closed = true;
-        incoming.destroy();
-    }
-    
-    c.on('error', function (err) { s.emit('close') });
-    c.on('data', function (buf) {
-        s.emit('data', buf);
-    });
-    
-    return s;
 }
