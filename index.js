f9998f6bf2379f695ac87f907996fdfc73d61d6e
Roman Shtylman <shtylman@gmail.com>
2012-12-16 23:58:30 -0500

    add pause/resume support to req.createRaw[Body]Stream

--- index.js
+++ index.js
@@ -16,32 +16,44 @@ function fromServer (Server, evName) {
         }
         var server = new Server(opts);
         server.on('request', function (req, res) {
+
             injectRaw(req);
+
+            // clear any previous buffered info
+            req.connection._rawBuffers = [];
+
             res.createRawStream = createWriteStream.bind(null, req);
             if (cb) cb.apply(this, arguments);
         });
         
         server.on(evName || 'connection', onconnection);
         server.on('upgrade', function (req) {
+
             injectRaw(req);
+
+            // clear any previous buffered info
+            req.connection._rawBuffers = [];
         });
         return server;
     };
 }
 
 function onconnection (con) {
-    var buffers = con._rawBuffers = [];
+    con._rawBuffers = [];
     
     var ondata = con.ondata;
     con.ondata = function (buf, start, end) {
-        buffers.push([ buf, start, end ]);
+        var copy = new Buffer(end - start);
+        buf.copy(copy, 0, start, end);
+
+        con._rawBuffers.push(copy);
+
         if (!con._upgraded) return ondata.apply(this, arguments);
     };
     
     var onend = con.onend;
     con.onend = function () {
-        buffers = undefined;
-        con._rawBuffers = undefined;
+        con._rawBuffers = [];
         if (!con._upgraded) return onend.apply(this, arguments);
     };
 }
@@ -52,20 +64,15 @@ function injectRaw (req) {
     req.createRawStream = function () {
         req.connection._upgraded = true;
         
-        var bufs = buffers;
         var s = createReadStream(req);
         s.buffers = buffers;
-        
-        if (buffers) process.nextTick(function () {
-            for (var i = 0; i < bufs.length; i++) {
-                var b = bufs[i];
-                if (b[2] - b[1] > 0) {
-                    s.emit('data', b[0].slice(b[1], b[2]));
-                }
+
+        process.nextTick(function () {
+            if (s._pause) {
+                return;
             }
-            bufs = undefined;
-            s.buffers = undefined;
-            req.connection._rawBuffers = undefined;
+
+            s.resume();
         });
         
         return s;
@@ -73,35 +80,42 @@ function injectRaw (req) {
     
     req.createRawBodyStream = function () {
         req.connection._upgraded = true;
+
+        var s = createReadStream(req);
         
-        var bufs = buffers;
-        if (buffers) process.nextTick(function () {
-            var b = bufs[bufs.length-1];
-            var str = String(b[0].slice(b[1],b[2]))
-            
+        if (buffers.length > 0) {
+            var b = buffers[buffers.length-1];
+            var str = String(b);
+
             var ix = str.indexOf('\r\n\r\n');
             if (ix >= 0) {
                 ix += 4
             }
             else {
                 ix = str.indexOf('\n\n');
-                if (ix < 0) return;
+                if (ix < 0) return s;
                 ix += 2;
             }
-            
+
             var b = str.slice(ix);
             if (b.length > 0) {
-                s.emit('data', Buffer(b));
+                // store to use in .resume if needed
+                s.body_buf = Buffer(b);
             }
+        }
+
+        // raw buffers no longer needed
+        buffers = [];
+        s.buffers = [];
+
+        process.nextTick(function () {
+            if (s._pause) {
+                return;
+            }
+
+            s.resume();
         });
-        
-        var s = createReadStream(req);
+
         return s;
     };
-    
-    process.nextTick(function () {
-        buffers = undefined;
-        req.connection._rawBuffers = undefined;
-    });
-    
 }
